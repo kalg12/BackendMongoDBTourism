74448125367cea36d546d3ae2df2b145b7bf87b4
Kevin Luciano <78965771+kalg12@users.noreply.github.com>
2023-06-28 11:15:00 -0600

    Routes

--- source/routes/departamentos.routes.js
+++ source/routes/departamentos.routes.js
@@ -1,1 +1,11 @@
-/* Continuar con la creación de rutas y revisión de enpoints con Postman */
+const express = require("express");
+const router = express.Router();
+const departamentosController = require("../controllers/departamentos.controllers");
+
+router.post("/", departamentosController.crearDepartamento);
+router.get("/", departamentosController.obtenerDepartamentos);
+router.get("/:id", departamentosController.obtenerDepartamentoPorId);
+router.put("/:id", departamentosController.actualizarDepartamento);
+router.delete("/:id", departamentosController.eliminarDepartamento);
+
+module.exports = router;
